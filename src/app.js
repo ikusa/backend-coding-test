fd3fd4d34a5835be44fdad977ce925615d0a9325
Joshua <joshuasetiawan04@gmail.com>
2019-07-29 23:31:44 +0700

    add swager ui and doc endpoint

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 const app = express();
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 module.exports = (db) => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
     app.get('/health', (req, res) => res.send('Healthy'));
 
     app.post('/rides', jsonParser, (req, res) => {
